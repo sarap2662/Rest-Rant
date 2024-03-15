38016797df412a727a623437546e2550644a50ab
sara pena <sarap@Saras-MacBook-Air.local>
2024-03-14 20:36:17 -0700

    Reordered folders

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('home')
-  })
-  
-  app.get('*', (req, res) => {
-    res.render('error404')
-  })
-  
-   
+  res.render('home')
+})
+
+app.get('*', (req, res) => {
+  res.render('error404')
+})
+
 app.listen(process.env.PORT)
