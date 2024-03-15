9f1ace41dec09b1467c8537a1f9f4e776fe2e83b
sara pena <sarap@Saras-MacBook-Air.local>
2024-03-14 23:49:14 -0700

    Adding styles to Rest-Rant

--- views/default.jsx
+++ views/default.jsx
@@ -1,16 +1,18 @@
 const React = require('react')
 
-function Def (html) {
+function Def(html) {
     return (
         <html>
             <head>
                 <title>Title</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-  }
-  
+}
+
 module.exports = Def
