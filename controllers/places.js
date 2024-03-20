f04d5b47ca322547125317a127b7a6e3e9127b5d
sara pena <sarap@Saras-MacBook-Air.local>
2024-03-19 21:13:36 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -1,17 +1,44 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+// GET /places
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.get('/', (req, res) => {
-  res.render('places/index', { places })
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id })
+  }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id], id })
+  }
+})
+
+
+
 router.post('/', (req, res) => {
-  console.log(req.body)
   if (!req.body.pic) {
     // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
@@ -26,9 +53,48 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    // Dig into req.body and make sure data is valid
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
 
-module.exports = router
+    // Save the new data into places[id]
+    places[id] = req.body
+    res.redirect(`/places/${id}`)
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
 
 
 
+module.exports = router 
 
--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
-app.use(express.urlencoded({ extended: true }))
+const methodOverride = require('method-override')
 
 // Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
-
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -24,4 +24,4 @@ app.get('*', (req, res) => {
 })
 
 // Listen for Connections
-app.listen(process.env.PORT)
+app.listen(process.env.PORT) 
