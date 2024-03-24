aeed122818c607f422c4997fbb4dfa1f46ac13b1
sara pena <sarap@Saras-MacBook-Air.local>
2024-03-23 19:00:18 -0700

    Started mongo changes

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -26,6 +26,11 @@ function new_form() {
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
                         <input className="form-control" id="cuisines" name="cuisines" required />
+                    <div className="form-group">
+                            <label for="founded">Founded Year</label>
+                            <input className="form-control" id="founded" name="founded" />
+                        </div>
+
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
                 </form>
