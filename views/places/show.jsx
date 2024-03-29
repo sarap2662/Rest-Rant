34a572d3fec33805b32734f94fca5572b2877e73
sara pena <sarap@Saras-MacBook-Air.local>
2024-03-28 23:51:32 -0700

    Added Seeders

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,17 +5,32 @@ function show(data) {
     return (
         <Def>
             <main>
-                <h1>{data.place.name}</h1>
-                <h2>Rating</h2>
-                <p>Not Rated</p>
-                <h2>Comments</h2>
-                <p>No comments yet</p>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <h1>{data.place.name}</h1>
+                        <h2>Rating</h2>
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                        <p>Not Rated</p>
+                        <h2>Description</h2>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                        <p>No comments yet</p>
+                    </div>
+                </div>
                 <a href={
-                    `/places/${data.id}/edit`} className="btn btn-warning">
+                    `/places/${data.id}/edit
+`} className="btn btn-warning">
                     Edit
                 </a>
                 <form method="POST" action={
-                    `/places/${data.id}?_method=DELETE`}>
+                    `/places/${data.id}?_method=DELETE
+`}>
                     <button type="submit" className="btn btn-danger">
                         Delete
                     </button>
